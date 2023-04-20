12c1bc8d6639a85978b7e42cfceee60b3fb73be1
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-04-21 02:22:23 +0800

    fixed error

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ import { Configuration, OpenAIApi } from "openai";
 
 dotenv.config();
 
+const app = express();
+
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
