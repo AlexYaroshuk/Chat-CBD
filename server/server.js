a72e8c352267c6b1d4be1852f38e025191ce101a
Alex Yaroshuk <34632190+AlexYaroshuk@users.noreply.github.com>
2023-05-01 15:46:42 +0800

    Feature/image (#4)
    * update server.js
    
    * upd cors w/ localhost
    
    * update server.js
    
    * added Access-Control-Allow-Origin
    
    * build: update package.json
    
    * trying ti allow all origins
    
    * update server.js
    
    * included isImage flag in response
    
    * modified image size key
    
    * update server.js w/ cors (retry)
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * added types in body/response
    
    * json stringify for image array
    
    * update server.js
    
    * added firebase writes
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * modified:   server/server.js
    
    * update server.js
    
    * more detailed error handling
    
    * update server.js
    
    * package/explicit bucket
    
    * bucket rewording
    
    * update server.js
    
    * upd bucket
    
    * update server.js
    
    * revert server.js
    
    * update server.js
    
    * added userid as argument
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * debugging
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * removed overwriting of chatHistory
    
    * update server.js
    
    * update server.js
    
    * fixed history overwrite bug
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * update server.js
    
    * better error handling
    
    * updated to prep for prod

--- server/server.js
+++ server/server.js
@@ -2,6 +2,24 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import { Configuration, OpenAIApi } from "openai";
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+const admin = require("firebase-admin");
+
+import fetch from "node-fetch";
+import FormData from "form-data";
+import fs from "fs";
+import { v4 as uuidv4 } from "uuid";
+
+const serviceAccountPath = "/etc/secrets/FIREBASE_SERVICE_ACCOUNT";
+const serviceAccountContent = fs.readFileSync(serviceAccountPath, "utf-8");
+const serviceAccount = JSON.parse(serviceAccountContent);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+});
 
 dotenv.config();
 
@@ -13,10 +31,7 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const allowedOrigins = [
-  "https://chat-cbd-test.vercel.app",
-  "https://chat-cbd.vercel.app",
-];
+const allowedOrigins = ["https://chat-cbd.vercel.app/"];
 
 const corsOptions = {
   origin: function (origin, callback) {
@@ -34,55 +49,166 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(express.json());
 
+async function uploadImageToFirebase(imageUrl) {
+  try {
+    const response = await fetch(imageUrl);
+    const buffer = await response.buffer();
+    const uniqueId = uuidv4();
+    const fileExtension = imageUrl.split(".").pop().split("?")[0];
+    const filename = `${uniqueId}.${fileExtension}`;
+
+    const file = admin.storage().bucket().file(filename);
+    const writeStream = file.createWriteStream({
+      metadata: {
+        contentType: response.headers.get("content-type"),
+      },
+    });
+
+    const uploadPromise = new Promise((resolve, reject) => {
+      writeStream.on("error", (error) => reject(error));
+      writeStream.on("finish", () => {
+        file.getSignedUrl(
+          {
+            action: "read",
+            expires: "03-17-2025",
+          },
+          (error, url) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(url);
+            }
+          }
+        );
+      });
+    });
+
+    writeStream.end(buffer);
+
+    const uploadedImageUrl = await uploadPromise.catch((error) => {
+      console.error("Error uploading image:", error);
+      throw error;
+    });
+
+    return uploadedImageUrl;
+  } catch (error) {
+    console.error("Error uploading image:", error);
+    throw error;
+  }
+}
+
 const PORT = process.env.PORT || 5000;
 
-app.get("/", async (req, res) => {
-  res.status(200).send({ message: "hello" });
-});
+function preprocessChatHistory(messages) {
+  return messages.map((message) => {
+    // Check if the message is an image
+    const isImage = message.type === "image";
+
+    // Only return the role and content properties of each message
+    return {
+      role: message.role,
+      content: isImage ? "generated image" : message.content,
+    };
+  });
+}
 
 try {
   app.post("/send-message", async (req, res) => {
     try {
-      const { messages } = req.body; // Receive the 'messages' object
-      console.log("Request payload:", req.body);
+      const { messages, type, activeConversation, userId } = req.body;
 
-      const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: messages, // Use the 'messages' object directly
-        temperature: 0.5,
-        max_tokens: 2000,
-        top_p: 1,
-        frequency_penalty: 0.5,
-        presence_penalty: 0,
-      });
+      const preprocessedMessages = preprocessChatHistory(messages);
 
-      console.log("OpenAI API response:", response);
+      let newMessage;
 
-      const botResponse = response.data.choices[0].message.content.trim();
+      if (type === "image") {
+        const imageResponse = await openai.createImage({
+          prompt: messages[messages.length - 1].content,
+          n: 1,
+          size: "256x256",
+          response_format: "url",
+        });
 
-      res.status(200).send({
-        bot: botResponse,
-        chatHistory: [...messages, { role: "system", content: botResponse }],
-      });
+        const imageUrl = imageResponse.data.data[0].url;
+        const uploadedImageUrl = await uploadImageToFirebase(imageUrl);
+
+        newMessage = {
+          role: "system",
+          content: "",
+          images: [uploadedImageUrl],
+          type: "image",
+        };
+
+        res.status(200).send({
+          bot: "",
+          type: "image",
+          images: [uploadedImageUrl],
+        });
+      } else {
+        const response = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: preprocessedMessages,
+          temperature: 0.5,
+          max_tokens: 2000,
+          top_p: 1,
+          frequency_penalty: 0.5,
+          presence_penalty: 0,
+        });
+
+        const botResponse = response.data.choices[0].message.content.trim();
+
+        newMessage = {
+          role: "system",
+          content: botResponse,
+          type: "text",
+        };
+
+        res.status(200).send({
+          bot: botResponse,
+          type: "text",
+        });
+      }
+
+      // Update the messages array with the new message
+
+      const updatedMessages = [...messages, newMessage];
+
+      await saveConversationToFirebase(
+        { id: activeConversation, messages: updatedMessages },
+        userId
+      );
     } catch (error) {
-      console.error(error);
       const { response } = error;
       let errorMessage = "An unknown error occurred";
+      let statusCode = 500; // Add this line to send the correct status code
 
       if (response && response.data && response.data.error) {
         errorMessage = response.data.error.message;
+        statusCode = response.status || 500; // Update the status code if available
       }
-
-      res.status(500).send({ error: errorMessage });
+      res.status(statusCode).send({ error: errorMessage }); // Send the status code along with the error message
     }
   });
 } catch (error) {
-  console.error("Unhandled error:", error); // Log the unhandled error
-  res.status(500).send({ error: "An unknown error occurred" });
+  let errorMessage = "An unknown error occurred";
+  let statusCode = 500;
+
+  if (error.response && error.response.data && error.response.data.error) {
+    errorMessage = error.response.data.error.message;
+    statusCode = error.response.status || 500;
+  }
+
+  res.status(statusCode).send({ error: errorMessage });
 }
 
-app.listen(process.env.PORT || 5000, () =>
-  console.log(
-    `Server is running on port http://localhost:${process.env.PORT || 5000}`
-  )
-);
+async function saveConversationToFirebase(conversation, userId) {
+  try {
+    const db = admin.firestore();
+    const conversationsRef = db.collection(`users/${userId}/conversations`);
+    const docRef = conversationsRef.doc(conversation.id);
+
+    await docRef.set(conversation);
+  } catch (error) {
+    console.error("Error saving conversation:", error);
+  }
+}
