7e3460256f62b6d51cbcc007e4b2009ca5ee6e33
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-05-07 21:54:56 +0800

    increased max_tokens

--- server/server.js
+++ server/server.js
@@ -149,7 +149,7 @@ try {
           model: "gpt-3.5-turbo",
           messages: preprocessedMessages,
           temperature: 0.5,
-          max_tokens: 2000,
+          max_tokens: 10000,
           top_p: 1,
           frequency_penalty: 0.5,
           presence_penalty: 0,
