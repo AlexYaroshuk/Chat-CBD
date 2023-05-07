592d8345da68bd16a2de8f50714cd4d27dede779
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-05-07 22:21:07 +0800

    revised max_tokens

--- server/server.js
+++ server/server.js
@@ -149,7 +149,7 @@ try {
           model: "gpt-4",
           messages: preprocessedMessages,
           temperature: 0.5,
-          max_tokens: 8192,
+          max_tokens: 2000,
           top_p: 1,
           frequency_penalty: 0.5,
           presence_penalty: 0,
