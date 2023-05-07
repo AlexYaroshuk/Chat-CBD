5f1a0a50d927f7da6159b902017609ab2862a912
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-05-07 22:11:50 +0800

    fixed error in max_tokens

--- server/server.js
+++ server/server.js
@@ -149,7 +149,7 @@ try {
           model: "gpt-4",
           messages: preprocessedMessages,
           temperature: 0.5,
-          max_tokens: 10000,
+          max_tokens: 8192,
           top_p: 1,
           frequency_penalty: 0.5,
           presence_penalty: 0,
