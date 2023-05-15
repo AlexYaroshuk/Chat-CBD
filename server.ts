61c6c343d15ee8c15ca1bcc69e99343647314d96
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-05-15 22:31:40 +0800

    update server.ts

--- server.ts
+++ server.ts
@@ -41,7 +41,10 @@ const stabilityApiHost = process.env.API_HOST ?? "https://api.stability.ai";
 const stabilityApiKey = process.env.STABILITY_API_KEY;
 //// ?*
 
-const allowedOrigins = ["https://chat-cbd.vercel.app"];
+const allowedOrigins = [
+  "https://chat-cbd-test.vercel.app",
+  "http://localhost:5173",
+];
 
 const corsOptions: CorsOptions = {
   origin: (
