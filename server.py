0ae6cff1daf59006a1f64e3b81f8ff8f822ba222
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-08-08 18:54:35 +0800

    update server.py

--- server.py
+++ server.py
@@ -14,7 +14,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/*": {"origins": allowed_origins}})
+
 
 # Firebase setup
 cred = credentials.Certificate("/etc/secrets/FIREBASE_SERVICE_ACCOUNT")
