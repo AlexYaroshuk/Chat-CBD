d58114b721415261192d4b4a7ab507c8913c354d
Alex Yaroshuk <alexander_4288@Yahoo.com>
2023-08-08 17:59:20 +0800

    added port

--- server.py
+++ server.py
@@ -254,4 +254,5 @@ def self_ping_scheduler():
 self_ping_scheduler()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
